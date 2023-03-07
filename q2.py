d90676255bc7069a9eda1fd798cc48854de0658d
eshavyas <vyase58@gmail.com>
2023-03-06 22:18:02 -0800

    done

--- q2.py
+++ q2.py
@@ -3,7 +3,11 @@
 # Make your Code
 # ******************************
 
+i = 4
 
+while i < 2:
+    print(i)
+    i += 1
 # Requirement
 # No need to use list
 # All input values are taken one by one separatively.
